43f5403fce6586818654fa516902c9efb127fa1c
Akram Khalid <akramkhalid12@gmail.com>
2020-10-10 20:10:31 -0500

    Starter files

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-import Cursor from "./cursor";
-import { gsap } from "gsap";
-
-const body = document.querySelector("body");
-
 window.onload = () => {
-  body.classList.remove("loading");
-  gsap.from(body, {
-    opacity: 0,
-    duration: 1,
-    ease: "Power3.easeInOut",
-  });
-  const cursor = new Cursor(document.querySelector(".cursor"));
+  console.log("hey there cutie ;)");
 };
